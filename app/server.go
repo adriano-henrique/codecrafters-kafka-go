a5fce1faf3e0d804f99f7cbb3c922f0f35708eb1
adriano-henrique <adrianohenriqueafonso@gmail.com>
2024-09-16 18:41:13 +0000

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -9,6 +9,14 @@ import (
 
 var _ = net.Listen
 var _ = os.Exit
+var validAPIVersions = []uint16{0, 1, 2, 3, 4}
+
+type ResponseType uint8
+
+const (
+	ResponseTypeError ResponseType = iota
+	ResponseTypeCorrelationID
+)
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
@@ -34,11 +42,18 @@ func main() {
 
 			_, headerBytes, err := parseMessage(c)
 
+			apiVersion := headerBytes[2:4]
+			if isValidAPIVersion(apiVersion) {
+				response := buildResponse(headerBytes, 0)
+				c.Write(response)
+			} else {
+				response := buildResponse(headerBytes, 35)
+				c.Write(response)
+			}
+
             if err != nil {
                 fmt.Println("Error parsing message:", err)
             }
-			response := buildResponse(headerBytes)
-			c.Write(response)
         }(conn)
     }
 }
@@ -57,12 +72,21 @@ func parseMessage(conn net.Conn) ([]byte, []byte, error) {
 	return lengthBytes, headerBytes, nil
 }
 
-func buildResponse(headerBytes []byte) []byte {
+func buildResponse(headerBytes []byte, errorCode int16) []byte {
 	responseLength := uint32(4)
-	response := make([]byte, 8)
+	response := make([]byte, 10)
 	var correlationID = binary.BigEndian.Uint32(headerBytes[4:8])
-
 	binary.BigEndian.PutUint32(response[0:4], responseLength)
 	binary.BigEndian.PutUint32(response[4:8], correlationID)
+	binary.BigEndian.PutUint16(response[8:10], uint16(errorCode))
 	return response
+}
+
+func isValidAPIVersion(apiVersion []byte) bool {
+	for _, validAPIVersion := range validAPIVersions {
+		if binary.BigEndian.Uint16(apiVersion) == validAPIVersion {
+			return true
+		}
+	}
+	return false
 }
